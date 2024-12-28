9a38e589817fba6b1eb8db585f3a8ab5e2818cd1
YuanZixuan06 <2996916287@qq.com>
2024-12-28 22:44:14 +0800

    Site updated: 2024-12-28 22:44:14

--- js/fomal.js
+++ js/fomal.js
@@ -290,7 +290,7 @@ function showWelcome() {
     // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
   }
 }
-window.onload = showWelcome;
+window.onload = showWelcome();
 // 如果使用了pjax在加上下面这行代码
 document.addEventListener('pjax:complete', showWelcome);
 
